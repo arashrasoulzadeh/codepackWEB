dd2fbf2dd3f4c60436794d7ed5185519b67f6120
arashrasoulzadeh <arashrasoulzadeh@gmail.com>
2016-11-07 13:48:31 +0330

    fixed reload import

--- sm/urls.py
+++ sm/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls import url
+from imp import reload
+
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
